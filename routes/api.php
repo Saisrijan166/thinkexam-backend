95535a1bfd27c24b71c597dc0c36db4f2d8d12ee
Saisrijan166 <srijansai166@gmail.com>
2025-02-10 13:25:10 +0530

    fri

--- routes/api.php
+++ routes/api.php
@@ -11,28 +11,12 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ProfileController;
 
 
-
-
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth:sanctum')->get('/profile', [ProfileController::class, 'getProfile']);
 Route::middleware('auth:sanctum')->post('/reset-password', [ProfileController::class, 'resetPassword']);
-
-// Route::get('test', function (Request $request) {
-//     return "correct";
-// });
-//     Route::get('list',[StudentController::class,'list']);
-
-// Route::group(['middleware' => 'auth:sanctum'], function () {
-//     Route::post('add',[StudentController::class,'add']);
-//     Route::get('delete/{id}',[StudentController::class,'delete']);
-//     Route::get('edit/{id}',[StudentController::class,'edit']);
-//     Route::put('editstudent/{id}',[StudentController::class,'editstudent']);
-//     Route::get('search',[StudentController::class,'search']);
-//     Route::post('deleteall',[StudentController::class,'deleteall']);
-// });
 
 
 Route::controller(UserAuthController::class)->group(function () {
@@ -80,7 +64,19 @@ Route::controller(ReportController::class)->group(function () {
 });
 
 
+// Route::get('test', function (Request $request) {
+//     return "correct";
+// });
+//     Route::get('list',[StudentController::class,'list']);
 
+// Route::group(['middleware' => 'auth:sanctum'], function () {
+//     Route::post('add',[StudentController::class,'add']);
+//     Route::get('delete/{id}',[StudentController::class,'delete']);
+//     Route::get('edit/{id}',[StudentController::class,'edit']);
+//     Route::put('editstudent/{id}',[StudentController::class,'editstudent']);
+//     Route::get('search',[StudentController::class,'search']);
+//     Route::post('deleteall',[StudentController::class,'deleteall']);
+// });
 
 
 
